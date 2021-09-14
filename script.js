e326f5307d6ac68d807948a8f3189c241f18dc2b
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2021-09-15 00:01:24 +0530

    is triangle completed

---
+++ script.js
@@ -0,0 +1,26 @@
+const Angles=document.querySelectorAll('.angle-inp');
+const submitBtn=document.querySelector('#submit-button');
+const outputEl=document.querySelector('#output');
+
+
+
+submitBtn.addEventListener('click', isTriangle);
+
+function isTriangle(){
+    const sumAngles=calculateSumOfAngles(Number(Angles[0].value),Number(Angles[1].value),Number(Angles[2].value))
+    if(sumAngles===180){
+        outputEl.innerText="Yaayy! The angles form a triangle";
+    }else if(sumAngles==""){
+        outputEl.innerText="Please enter all the fields!🙄";
+    }
+    else{
+        outputEl.innerText="Oh oh! The angles doesn't form a triangle";
+    }
+
+}
+
+function calculateSumOfAngles(angle1,angle2,angle3){
+    const sumOfAngles=(angle1+angle2+angle3);
+    return sumOfAngles;
+
+}
