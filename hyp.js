fa4d293f155071e92d73cc561764824bd6248492
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2021-09-15 11:53:26 +0530

    completed hyp

---
+++ hyp.js
@@ -0,0 +1,20 @@
+const b1=document.querySelector('#base');
+const h1=document.querySelector('#height');
+const submitBtn=document.querySelector('#submit-button');
+const outputEl=document.querySelector('#output');
+
+submitBtn.addEventListener('click', calculateHypotenuse);
+
+function calculateHypotenuse(){
+    const base=Number(b1.value);
+    const height=Number(h1.value);
+    // const hyp=Math.sqrt(base*base+height*height);
+    let hyp;
+
+    if(hyp=Math.sqrt(base*base+height*height)){
+        outputEl.innerText=`The length of the hypotenuse is ${hyp}`;
+     }else{
+        outputEl.innerText="The length of the hypotenuse is 0";
+     }
+
+}
